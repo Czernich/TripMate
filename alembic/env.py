687774b64a8bf6f6f89a06f8d1eb47b15b188e36
kuligambino01 <czaro923@gmail.com>
2026-09-26 18:19:15 +0200

    TPM-2: merge migrations heads

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from app.models.base import Base
+from app.models.trip import Trip
 from app.settings import settings_db
 
 
